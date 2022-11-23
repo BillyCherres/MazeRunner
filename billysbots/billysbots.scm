48655579f041f98aa2879cefdb360705445b81ff
KJohanson03 <118920523+KJohanson03@users.noreply.github.com>
2022-11-23 10:54:56 -0600

    Add files via upload

---
+++ billysbots/billysbots.scm
@@ -0,0 +1,109 @@
+; 
+; CSC 151 Group project final
+;
+; Kevin Johanson, Breck Bishop, Billy Cherries
+;
+; 
+; Acknowledgements: Some Code taken from mini proj 6
+;
+;
+
+
+
+
+; step 1: figure out how to take an audio input
+; step 2: import mini proj 6 that plays a sound based on a vector
+; step 3: make new sound envelopes for the voice changer options
+;
+;
+
+; *************** import mini proj 6 *****************
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;
+;********************************************************
+
+
+
+
+
+
+
+
+(define deep-voice-envelope 
+(lambda (end)
+"vector"
+))
+
+
+(define high-voice-envelope 
+(lambda (end)
+"vector"
+))
+
+
+(define robot-voice-envelope 
+(lambda (end)
+"vector"
+))
+
+
+(define slow-voice-envelope 
+(lambda (end)
+"vector"
+))
+
+(define fast-voice-envelope 
+(lambda (end)
+"vector"
+))
+
+
+(define delayed-voice-envelope 
+(lambda (end)
+"vector"
+))
+
+(define overlayed-voice-envelope 
+(lambda (end)
+"vector"
+))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
