e8bbafdfb5a1af4548365cd8af7815374909d29a
Cherres <cherresb@easley.cs.grinnell.edu>
2022-12-06 20:11:30 -0600

    color changer

--- maze.scm
+++ maze.scm
@@ -21,9 +21,9 @@
 (button "button" "reset button"))
 ; ========================creating the button's fucntionalities=================================
 
-(define positionx  (vector 280)
+(define positionx  (vector 0)
 )
-(define positiony (vector 480))
+(define positiony (vector 100))
 (button-onclick upbtn
 (lambda ()
 (vector-set! positiony 0 (- (vector-ref positiony 0 )20 ))
@@ -50,21 +50,21 @@
 
 (button-onclick resetbtn
 (lambda ()
-(begin (vector-set! positionx 0 250) (vector-set! positiony 0 480))))
+(begin (vector-set! positionx 0 0) (vector-set! positiony 0 100))))
 
 
 ; -========================creating the canavas================================
-(define cvs (canvas 560 500))
+(define cvs (canvas 560 560))
 
 
 (define backround-color 
-(vector "black")
+(vector "green")
 )
 
 (animate-with 
 (lambda (time)
 (begin 
-(rectangle cvs 0 0 560 500 "solid" (vector-ref backround-color 0))
+(rectangle cvs 0 0 560 560 "solid" (vector-ref backround-color 0))
 (rectangle cvs (vector-ref positionx 0) (vector-ref positiony 0) 20 20 "solid" "red")
 )
 ))
