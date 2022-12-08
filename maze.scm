eb43a9fddd875aaf257f00903f2663692c52ee0e
Billy.C <72890070+BillyCherres@users.noreply.github.com>
2022-12-08 15:10:20 -0600

    created the color changing box and entrance/exit

--- maze.scm
+++ maze.scm
@@ -2,64 +2,82 @@
 
 (import canvas)
 (import html)
+(import music)
 
-
+(note 67 qn)
 
 ; ========================creating the buttons=================================
 (define upbtn
-(button "button" "up arrow")
-)
+  (button "button" "up arrow")
+  )
 (define downbtn 
-(button "button" "down arrow")
-)
+  (button "button" "down arrow")
+  )
 (define leftbtn 
-(button "button" "left arrow")
-)
+  (button "button" "left arrow")
+  )
 (define rightbtn 
-(button "button" "right arrow")
-)
+  (button "button" "right arrow")
+  )
 (define resetbtn
-(button "button" "reset button"))
+  (button "button" "reset button"))
 (define cheatbtn
-(button "button" "cheat"))
+  (button "button" "cheat"))
+
+
+
 ; ========================creating the button's fucntionalities=================================
 
-(define positionx  (vector 0)
-)
+(define positionx (vector 0)
+  )
 (define positiony (vector 50))
+; =======================Changing colors of the maze and peices==========================
+
+(define color-bank
+  (vector "orange" "yellow" "purple" "white"))
+
+(define color-of-square
+  (vector "red")
+  )
+
+(define color-changer
+  (lambda ()
+    (vector-set! color-of-square 0 (vector-ref color-bank (random (vector-length color-bank))) )
+    )
+  )
 
 
+; =============================================================
 (button-onclick upbtn
-(lambda ()
-(vector-set! positiony 0 (- (vector-ref positiony 0 )10 ))
+  (lambda ()
+    (begin (vector-set! positiony 0 (- (vector-ref positiony 0 )10 ) ) (color-changer))) )
 
-))
 
 (button-onclick downbtn
-(lambda ()
-(vector-set! positiony 0 (+ (vector-ref positiony 0 )10 ))
-
-))
+  (lambda ()
+    (begin (vector-set! positiony 0 (+ (vector-ref positiony 0 )10 ))
+      (color-changer)
+      )))
 
 (button-onclick leftbtn
-(lambda ()
-(vector-set! positionx 0 (- (vector-ref positionx 0 ) 10 ))
-
-))
+  (lambda ()
+    (begin (vector-set! positionx 0 (- (vector-ref positionx 0 ) 10 ))
+      (color-changer)
+      )))
 
 (button-onclick rightbtn
-(lambda ()
-(vector-set! positionx 0 (+ (vector-ref positionx 0 ) 10 ))
-
-))
+  (lambda ()
+    (begin (vector-set! positionx 0 (+ (vector-ref positionx 0 ) 10 ))
+      (color-changer)
+      )))
 
 (button-onclick resetbtn
-(lambda ()
-(begin (vector-set! positionx 0 0) (vector-set! positiony 0 100))))
+  (lambda ()
+    (begin (vector-set! positionx 0 0) (vector-set! positiony 0 50) (color-changer))))
 
 (button-onclick cheatbtn
-(lambda ()
-(begin (vector-set! positionx 0 400) (vector-set! positiony 0 490))))
+  (lambda ()
+    (begin (vector-set! positionx 0 400) (vector-set! positiony 0 490) (color-changer))))
 ; -========================creating the canavas================================
 (define cvs (canvas 560 560))
 
@@ -69,27 +87,27 @@
   )
 
 
-(define color-of-rectangle 
-  (vector "black")
-  )
 
 
 (define color-of-inside-walls
   "green"
-)
+  )
 (define color-of-otuside-walls
   "blue"
-)
+  )
+
+
 
 
 (animate-with 
-(lambda (time)
-(begin 
+  (lambda (time)
+    (begin 
       ;--------- background color= ------------
+      
       (rectangle cvs 0 0 560 560 "solid" (vector-ref color-of-rectangle 0))
       ;--------- position of the users box (red square) ------------
       
-      (rectangle cvs (vector-ref positionx 0) (vector-ref positiony 0) 20 20 "solid" "red")
+      (rectangle cvs (vector-ref positionx 0) (vector-ref positiony 0) 20 20 "solid" (vector-ref color-of-square 0))
       ;--------- outer wall ------------
       (rectangle cvs 40 70 340 10 "solid" color-of-inside-walls)
       (rectangle cvs 410 70 110 10 "solid" color-of-inside-walls)
@@ -100,6 +118,8 @@
       (rectangle cvs 370 110 10 140 "solid" color-of-inside-walls)
       (rectangle cvs 410 150 10 50 "solid" color-of-inside-walls)
       (rectangle cvs 410 150 50 10 "solid" color-of-inside-walls)
+      (rectangle cvs 30 40 10 30 "solid" "red")
+      
       ;------------------ right spiral---------------
       (rectangle cvs 70 110 350 10 "solid" color-of-inside-walls)
       (rectangle cvs 70 110 10 140 "solid" color-of-inside-walls)
@@ -130,7 +150,6 @@
       (rectangle cvs 340 410 10 80 "solid" color-of-inside-walls)
       (rectangle cvs 340 480 140 10 "solid" color-of-inside-walls)
       (rectangle cvs 480 410 10 80 "solid" color-of-inside-walls)
-
       (rectangle cvs 440 410 10 40 "solid" color-of-inside-walls)
       (rectangle cvs 390 410 10 40 "solid" color-of-inside-walls)
       ; -----------------start of maze builiding --------------------------------------
@@ -140,8 +159,8 @@
       (rectangle cvs 520 40 10 480 "solid" color-of-otuside-walls)
       (rectangle cvs 520 410 10 30 "solid" "black")
       (rectangle cvs 400 400 40 10 "solid" "black")
-)
-))
+      (rectangle cvs 520 410 10 30 "solid" "red"))
+    ))
 
 
 
